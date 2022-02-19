8ce4bbee5123a3d01f8f3c24c9713a3f4fb10c81
BroadSide3 <91118804+BroadSide3@users.noreply.github.com>
2022-02-20 02:46:00 +0300

    JS testing file
    Added javascript folder w/ js file

---
+++ src/javascript/js.js
@@ -0,0 +1,32 @@
+// $.post('/api/formatter/formatter.py', {query: 'test'}, function(data) {
+//     console.log(data); // ответ от сервера
+// })
+// .success(function() { console.log('Успешное выполнение'); })
+// .error(function(jqXHR) { console.log('Ошибка выполнения'); })
+// .complete(function() { console.log('Завершение выполнения'); });
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('POST','/api/formatter/formatter.py');
+xhr.send();
+
+xhr.onload = function() {
+  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+  } else { // если всё прошло гладко, выводим результат
+    alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
+  }
+};
+
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    alert(`Получено ${event.loaded} из ${event.total} байт`);
+  } else {
+    alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+  }
+
+};
+
+xhr.onerror = function() {
+  alert("Запрос не удался");
+};
