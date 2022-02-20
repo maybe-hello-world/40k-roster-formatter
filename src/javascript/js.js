82e0845102b6b669fda36b3f59e274cb9f31d50a
BroadSide3 <91118804+BroadSide3@users.noreply.github.com>
2022-02-20 23:12:19 +0300

    Update js.js
    Added status-message for testing post request

--- src/javascript/js.js
+++ src/javascript/js.js
@@ -7,3 +7,14 @@ let formData = new FormData(document.forms.formatter);
 
  xhr.onload = () => alert(xhr.response);
 
+xhr.upload.onprogress = function(event) {
+  alert(`Отправлено ${event.loaded} из ${event.total} байт`);
+};
+
+xhr.upload.onload = function() {
+  alert(`Данные успешно отправлены.`);
+};
+
+xhr.upload.onerror = function() {
+  alert(`Произошла ошибка во время отправки: ${xhr.status}`);
+};
